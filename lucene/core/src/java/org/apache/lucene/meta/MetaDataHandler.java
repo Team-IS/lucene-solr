1abe4510e4bb17cdc19f6fb9b55916c7233e3102
pitsios-s <stamatis.pit@gmail.com>
2014-05-02 13:03:32 +0300

    Modified class MetaDataHandler.
    Due to bugs found during testing, we added some extra if-statements in order not to return empty Strings.

--- lucene/core/src/java/org/apache/lucene/meta/MetaDataHandler.java
+++ lucene/core/src/java/org/apache/lucene/meta/MetaDataHandler.java
@@ -58,7 +58,7 @@ public class MetaDataHandler extends DefaultHandler {
    }
    
    @Override
-       public void startElement(String uri , String localName , String qName , Attributes atts) throws SAXException {
+   public void startElement(String uri , String localName , String qName , Attributes atts) throws SAXException {
        
       if(qName.equals("uses-version")) {
          this.parsingVersion = true;
@@ -75,17 +75,29 @@ public class MetaDataHandler extends DefaultHandler {
     
    @Override
    public void characters(char ch[], int start, int length) throws SAXException {
-       
+      
       if(this.parsingVersion) {
-         this.metadata[0] = new String(ch,start,length).trim();
+        String line = new String(ch,start,length).trim();
+        
+        if(!line.equals("")) {
+          this.metadata[0] = line;
+        }
       }
        
       else if(this.parsingAnalyzer) {
-         this.metadata[1] = new String(ch,start,length).trim();
+        String line = new String(ch,start,length).trim();
+        
+        if(!line.equals("")) {
+          this.metadata[1] = line;
+        }
       }
        
       else if(this.parsingSimilarity) {
-         this.metadata[2] = new String(ch,start,length).trim();
+        String line = new String(ch,start,length).trim();
+        
+        if(!line.equals("")) {
+          this.metadata[2] = line;
+        }
       }
    }
 
